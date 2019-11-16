f2ffa6efb18b98f49823e4ff556003ab9c4cf32e
Bruno Vandekerkhove <Buno@MacBook-Bruno.local>
2019-11-16 19:31:32 +0100

    Opdracht 15-16

--- practicum_1/src/s0216676_predictedBestMovies.m
+++ practicum_1/src/s0216676_predictedBestMovies.m
@@ -1,4 +1,15 @@
 function [movieIDs,score] = s0216676_predictedBestMovies(Uk,sk,Vk)
-
+    [m,~] = size(Uk); 
+    [n,~] = size(Vk);
+    score = zeros(n, 1);
+    for i = 1:m
+        acc = 0;
+        x = Uk(i,:) .* sk';
+        for j = 1:n
+            acc = x * Vk(j,:)' + acc;
+        end
+        score(i) = acc / n;
+    end
+    [score, movieIDs] = sort(score, 'descend');
 end
 
