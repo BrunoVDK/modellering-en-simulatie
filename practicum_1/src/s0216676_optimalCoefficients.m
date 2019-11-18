d8bd46e12e1bceee9e94b044ffaa50a95439b304
Bruno Vandekerkhove <Buno@MacBook-Bruno.local>
2019-11-18 13:39:31 +0100

    Opdracht 6 + 12

--- practicum_1/src/s0216676_optimalCoefficients.m
+++ practicum_1/src/s0216676_optimalCoefficients.m
@@ -1,11 +1,9 @@
 function [s] = s0216676_optimalCoefficients(Uk,Vk,A)
-    [m,k] = size(Uk);
-    [n,~] = size(Vk);
-    B = sparse([], [], [], m * n, k, k * nnz(A)); % Allocate space
-    for j = 1:k
-        B(:,j) = reshape(s0216676_sparseModel(Uk(:,j), 1, Vk(:,j), A), [], 1); %#ok
+    B = repmat(spones(A(:)), 1, size(Uk,2));
+    for j = 1:size(Uk,2)
+        B(:,j) = reshape(s0216676_sparseModel(Uk(:,j), 1, Vk(:,j), A), [], 1);
     end
-    % s = lsqminnorm(B , A(:));
     [s,~] = lsqr(B, A(:));
+    % s = lsqminnorm(B , A(:));
     % s = lsqr(B, A(:), [], 10);
 end
